a2f2fbedcaf011b08f7e093519fa68cb737714fc
Relintai <relintai@protonmail.com>
2022-04-27 13:02:00 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# UML Generator
+
+Small UML generator using the pandemonium engine. Should work with godot asewell, but you will need to create a project file.
+
+
---
+++ module_config.py
@@ -0,0 +1,18 @@
+
+pandemonium_branch = 'master'
+
+engine_repository = [ ['https://github.com/Relintai/pandemonium_engine.git', 'git@github.com:Relintai/pandemonium_engine.git'], 'pandemonium_engine', '' ]
+
+module_repositories = [
+]
+
+removed_modules = [
+]
+
+addon_repositories = [
+]
+
+third_party_addon_repositories = [
+]
+
+slim_args = 'module_webm_enabled=no module_arkit_enabled=no module_visual_script_enabled=no module_gdnative_enabled=no module_mobile_vr_enabled=no module_theora_enabled=no module_xatlas_unwrap_enabled=no'
