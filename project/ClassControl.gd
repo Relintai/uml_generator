843ece8db861f3b61cf0890577c6b3538d76792d
Relintai <relintai@protonmail.com>
2022-04-27 15:09:17 +0200

    Add the double hseparators under the class name only when needed.

--- project/ClassControl.gd
+++ project/ClassControl.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+var _item_added : bool = false
+
 func set_class_name(n):
 	$VBoxContainer/HBoxContainer/ClassName.text = n
 
@@ -9,6 +11,11 @@ func add_h_separator():
 	$VBoxContainer.add_child(hsep)
 
 func add_line(line : String):
+	if !_item_added:
+		_item_added = true
+		add_h_separator()
+		add_h_separator()
+	
 	var l : Label = Label.new()
 	l.text = line
 	$VBoxContainer.add_child(l)
